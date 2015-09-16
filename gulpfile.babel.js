1fe6e92b227736f43f2d1b4bb48165562eafe4aa
Amie Wilt <amie.wilt@gmail.com>
2015-09-15 18:07:33 -0700

    app js files moving to dist folder in build, getting 'identified already declared' error

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -55,7 +55,7 @@ gulp.task('html', ['styles'], () => {
 
   return gulp.src('app/*.html')
     .pipe(assets)
-    .pipe($.if('scripts/*.js', $.uglify()))
+    .pipe($.if('.tmp/scripts/*.js', $.uglify()))
     .pipe($.if('styles/*.css', $.minifyCss({compatibility: '*'})))
     .pipe(assets.restore())
     .pipe($.useref())

3e358cd9b32f1bc93c03ff54aaa8340add8fb136
Amie Wilt <amie.wilt@gmail.com>
2015-09-23 12:09:10 -0700

    updated md copy, started on pw styling

--- app/scripts/mdAboutCtrl.js
+++ app/scripts/mdAboutCtrl.js
@@ -1,12 +1,16 @@
 class MdAboutCtrl {
-  constructor ($scope, mdAboutFac) {
-    $scope.description = mdAboutFac.description;
+  constructor($scope, mdAboutFac) {
+    $scope.descriptions = mdAboutFac.descriptions;
   }
 }
 
 var mdAboutFactory = () => {
   return {
-    description: 'Tony\'s Performance Services has been serving Sarasota with premium mobile detailing since 2007. Our service is exactly what it says--mobile. We come to you wherever you are. On top of that, we use our own water and electric.'
+    descriptions: [
+      'We have been serving Sarasota and the surrounding areas with premium mobile detailing since 2007. Our service is exactly what it says--mobile. We come to you wherever you are. On top of that, we use our own water and electric. We also understand the importance of a job well done, which is why we do 100% hand washing and waxing.',
+      'Our system is blah blah blah and blah blah. We use top of the line equipment and products to ensure your vehicle will be looking new and will be protected from the environment\'s harsh conditions. We don\'t believe this will be an issue, but if you feel our work isn\'t up to your standards, please notify us immediately and we will remedy the situation as soon as possible.',
+      'We are committed to serving our customers with the best detailing in the Sarasota area. Many thanks to our loyal customers for allowing us to do so and welcome to the new ones!'
+    ]
   };
 };
 
--- app/scripts/mdCtrl.js
+++ app/scripts/mdCtrl.js
@@ -14,7 +14,7 @@ var mdFactory = () => {
     descriptionTagline: 'Take pride in your ride.',
     descriptions: [
       'Everything needs maintenance, your vehicle is no exception. TPS provides premium mobile detailing services to preserve the value of your vehicle and to protect it from harmful contaminants. By keeping up with regular detailing, you\'ll get top dollar when it\'s time to trade or sell.',
-      'Our services range from a basic wash to a full detail for anything with a motor--even boats and planes. This includes waxing, carpet/upholstery shampooing and conditioning, headlight buffing and tire shining. The best part is that we are completely mobile, which means no interruptions in your schedule. We\'ll happily come to you wherever you are.',
+      'Our services range from a basic wash to a full detail for anything with a motor--even boats and planes. This includes waxing, carpet/upholstery shampooing and conditioning, headlight restoration and tire shining. The best part is that we are completely mobile, which means no interruptions in your schedule. We\'ll happily come to you wherever you are.',
       'For more information, check out our services and pricing, and be sure to visit our photo gallery for some before and afters. If you would like to schedule an appointment or have any questions or comments, please fill out our form or call or text 941.888.3533. We look forward to hearing from you!',
       'TPS'
     ]

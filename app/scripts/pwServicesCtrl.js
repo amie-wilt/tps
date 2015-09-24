e5b74a7b4a9554185e45513b4ef2aa30acf63904
Amie Wilt <amie.wilt@gmail.com>
2015-09-24 14:02:35 -0700

    worked on pw services copy and styling

--- app/scripts/pwServicesCtrl.js
+++ app/scripts/pwServicesCtrl.js
@@ -12,7 +12,7 @@ var pwServicesFactory = () => {
       'Sealing',
       'Re-Screening'
     ],
-    description: 'Phasellus odio velit, iaculis ut enim vitae, sodales laoreet dolor. Sed porta diam in nisl tincidunt, finibus maximus dolor rhoncus. Nulla quis sollicitudin nunc. Nulla tempus nisi augue, eu sodales tortor consequat eget. Fusce in mi non ex fermentum tincidunt.'
+    description: 'Prices vary depending on the job and are based on the condition and size of the property. We offer free estimates so please contact us at 941.333.5555 or fill out the form.'
   };
 };
 

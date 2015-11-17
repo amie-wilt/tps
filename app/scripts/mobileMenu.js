5d59bc61d8dfc6ce0f2f09e397b1161c5efa31dc
Amie Wilt <amie.wilt@gmail.com>
2015-11-17 18:32:32 -0500

    decreased sizes on mobile menu nav for small media query, fixed pressure washing navigation, took position fixed off of footer

--- app/scripts/mobileMenu.js
+++ app/scripts/mobileMenu.js
@@ -1,5 +1,41 @@
-/**
- * Created by Tim on 10/20/15.
- */
+class MobileMenu {
+  constructor(container) {
+    this.$container = container;
+    this.$nav = this.$container.find('[data-class="menu"]');
+    this.$menuToggle = this.$container.find('[data-class="mobile-menu-toggle"]');
+    this.activeClass = 'active';
+    this.validator = false;
 
+    this.$menuToggle.on('click', () => {
+      this.validate();
+    });
+  }
 
+  showMenu() {
+    this.$container.addClass(this.activeClass);
+    this.$menuToggle.addClass(this.activeClass);
+    this.validator = true;
+  }
+
+  hideMenu() {
+    this.$container.removeClass(this.activeClass);
+    this.$menuToggle.removeClass(this.activeClass);
+    this.validator = false;
+  }
+
+  validate() {
+    if (this.validator) {
+      this.hideMenu();
+      this.validator = false;
+    } else {
+      this.showMenu();
+      this.validator = true;
+    }
+  }
+}
+
+$(function() {
+
+  var mobileMenu = new MobileMenu($('[data-class="menu-container"]'));
+
+});
